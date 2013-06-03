061dec2fdb267a1506bda194db91482e63957c85
Dana O'Rourke <dana@cupofteacreations.com>
2013-06-02 17:24:28 -0700

    Some basic data and a skeleton.

--- index.php
+++ index.php
@@ -22,17 +22,7 @@
 	<script type="text/javascript" src="js/game.js"></script>
 	<script type="text/javascript" src="js/character.js"></script>
 	<script type="text/javascript" src="js/area.js"></script>
-	<script type="text/javascript" src="js/plant.js"></script>
-	<script type="text/javascript">
-	/*	var rose = new glob('Rose');
-		var jack = new glob('Jack');
-		var micky = new glob('Micky');
-		var ricky = new glob('Ricky');
-		var amy = new glob('Amy');
-		var rory = new glob('Rory');
-		var objects = new Array(rose,jack,micky,ricky,amy,rory);
-		game.init('gamespace',objects);*/
-		
+	<script type="text/javascript">		
 		var o = new Array(character,area);
 		game.init('gamespace',o);
 	</script>
